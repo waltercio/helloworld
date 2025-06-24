1ceed34babe87bd745d72d5a30972745985ce274
Waltercio De Oliveira Barros Da Costa <wobc@br.ibm.com>
2025-06-24 12:02:28 -0300

    try to fix initialization

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-mvn spring-boot:run
+echo "[INFO] Running with UID: $(id -u)"
 
-#tail -f /dev/null
+# Remova o target e classes criadas por usuários anteriores (caso existam)
+rm -rf target/
+
+# Rode o Maven (recria os arquivos com o UID do pod atual)
+mvn clean spring-boot:run
